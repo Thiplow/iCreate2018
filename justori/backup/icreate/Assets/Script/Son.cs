e936a7ea615cab212069836de087722a40ebec8d
Thibault Soignon <thib97@gmail.com>
2018-03-27 17:17:48 +0200

    Fichier sound

---
+++ justori/backup/icreate/Assets/Script/Son.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Leap.Unity
+{
+    public class Son : MonoBehaviour
+    {
+
+        [SerializeField]
+        public float volume;
+
+        [SerializeField]
+        public string pisteAudio;
+
+        [SerializeField]
+        public int id;
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+        void playSound()
+        {
+
+        }
+    }
+}
